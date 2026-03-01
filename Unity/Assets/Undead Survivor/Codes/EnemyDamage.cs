276763c9bc60d6386393d97000855b8b875aa4d3
paul1405dev-hub <paul1405.dev@gmail.com>
2026-03-01 16:23:42 +0900

    12(collider,damage)

---
+++ Unity/Assets/Undead Survivor/Codes/EnemyDamage.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    private const float Damage = 0.5f;
+
+    private void OnTriggerEnter2D(Collider2D other){
+
+        if (other.CompareTag("Player")) 
+        {
+            other.GetComponent<FarmerHp>().TakeDamage(Damage);
+        }
+    }
+}
+
---
+++ Unity/Assets/Undead Survivor/Codes/FarmerHp.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class FarmerHp : MonoBehaviour
+{
+    private const float MaxHp = 100f;
+    private float _currentHp = MaxHp;
+
+    public void TakeDamage(float damage){
+
+        _currentHp -= damage;
+        Debug.Log($"Farmer HP: {_currentHp}");
+    }
+}
+
