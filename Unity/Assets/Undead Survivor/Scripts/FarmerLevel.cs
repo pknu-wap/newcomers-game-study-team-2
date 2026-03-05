f970f1d6462aba3edc6eded4980b320947d48110
안소민 <somin1937@naver.com>
2026-03-05 22:21:49 +0900

    UI 추가

--- Unity/Assets/Undead Survivor/Scripts/FarmerLevel.cs
+++ Unity/Assets/Undead Survivor/Scripts/FarmerLevel.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class FarmerLevel : MonoBehaviour
 {
+    [SerializeField] private GameObject levelUpPanel;
     private int _currentExp;
     private int _level = 1;
     private int _maxExp = 50;
@@ -14,9 +15,15 @@ public class FarmerLevel : MonoBehaviour
             _currentExp -= _maxExp;
             _level++;
             _maxExp = _level * 50;
+            levelUpPanel.SetActive(true);
             TimeController.PauseGame();
         }
         
         Debug.Log("레벨: " + _level + " 경험치: " + _currentExp);
+    }
+    public void LevelUp(int index)
+    {
+        levelUpPanel.SetActive(false);
+        TimeController.ResumeGame();
     }
 }
