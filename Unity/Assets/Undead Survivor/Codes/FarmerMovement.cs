6659eabdd084f03ee55ed532e354d383ed72e564
paul1405dev-hub <paul1405.dev@gmail.com>
2026-03-06 13:52:43 +0900

    16,17(Farmer-Animation)

--- Unity/Assets/Undead Survivor/Codes/FarmerMovement.cs
+++ Unity/Assets/Undead Survivor/Codes/FarmerMovement.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,15 +7,47 @@ public class FarmerMovement : MonoBehaviour
 {
     private const float Speed = 10f;
     private Vector3 _moveDirection;
-
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private enum FarmerState { Stand, Run }
+    
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     private void Update()
     {
         transform.Translate(Time.deltaTime * Speed * _moveDirection);
-    }
+        if (_moveDirection.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (_moveDirection.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        
 
+        FarmerState state;
+        if (_moveDirection == Vector3.zero)
+        {
+            state = FarmerState.Stand;
+        }
+        else
+        {
+            state = FarmerState.Run;
+        }
+
+        animator.SetInteger("state", (int)state);
+    }
+    
     private void OnMove(InputValue value)
     {
         _moveDirection = value.Get<Vector2>();
         _moveDirection.Normalize();
     }
+
+
+
 }
